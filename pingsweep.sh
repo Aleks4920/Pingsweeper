7eb3149b0a9a1ec119583f631ee2056f5cb617f1
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2021-04-28 14:42:28 -0400

    Add files via upload

---
+++ pingsweep.sh
@@ -0,0 +1,11 @@
+sed -i -e 's/\r$//' pingsweep.sh
+#! /usr/bin/bash
+
+if [ "$1" == "" ]
+then
+  echo "provide ip address"
+else
+for ip in `seq 1 254` ; do
+  ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
+fi
